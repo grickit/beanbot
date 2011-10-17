1704e2c31cb3b6dcd3dd439c78946616c8b827ea
Derek Hoagland <thegrickit@gmail.com>
2011-10-17 11:09:39 -0400

    Setting up the core variables I'll need later.

--- beanbot.java
+++ beanbot.java
@@ -2,45 +2,25 @@ import java.io.*;
 import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
+import java.util.*;
 
 public class beanbot {
 
   private static SocketChannel serverConnection;
-  private static ByteBuffer toServer;
-  private static ByteBuffer fromServer;
+  private static ByteBuffer toServer = ByteBuffer.allocateDirect(1024);
+  private static ByteBuffer fromServer = ByteBuffer.allocateDirect(1024);
+  private static Hashtable<String,String> core = new Hashtable<String,String>();
+  private static Hashtable<String,String> config = new Hashtable<String,String>();
+  private static Hashtable<String,String> variables = new Hashtable<String,String>();
+  private static Hashtable<String,String> persistent = new Hashtable<String,String>();
 
-  public static void main(String[] args) throws IOException, InterruptedException {
+  private static Hashtable<String,Integer> pidpipes = new Hashtable<String,Integer>();
+  private static Hashtable<String,ByteBuffer> readpipes = new Hashtable<String,ByteBuffer>();
+  private static Hashtable<String,ByteBuffer> writepipes = new Hashtable<String,ByteBuffer>();
 
-    toServer = ByteBuffer.allocateDirect(1024);
-    fromServer = ByteBuffer.allocateDirect(1024);
-    serverConnection = createConnection("chat.freenode.net",6667);
-    login();
-
-    while(true) {
-      sleep(100);
-      fromServer.clear();
-      int numberBytesRead = serverConnection.read(fromServer);
-
-      if (numberBytesRead == -1) {
-	System.out.println("IRC connection died.");
-	serverConnection = createConnection("chat.freenode.net",6667);
-	login();
-      }
-      else {
-	String incoming = "";
-	fromServer.flip();
-	byte[] bytes = new byte[fromServer.remaining()];
-	fromServer.get(bytes);
-
-	if(bytes.length > 0) {
-	  String s = new String(bytes,"UTF-8");
-	  incoming = incoming + s;
-	  //TODO: Handle incoming message
-	  System.out.println(incoming);
-	}
-      }
-    }
-  }
+  private static String[] pending_outgoing;
+  private Date last_second = new Date();
+  private int messages_this_second = 0;
 
   public static void sleep(int millis) throws InterruptedException {
     try {
@@ -80,5 +60,39 @@ public class beanbot {
     sendServerMessage("NICK Gambeanbot\n");
     sendServerMessage("USER Gambot 8 * :Java Gambot\n");
     sendServerMessage("JOIN ##Gambot\n");
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    core.put("home_directory",new java.io.File("").getAbsolutePath());
+    core.put("configuration_file","config.txt");
+    core.put("message_count","0");
+
+    serverConnection = createConnection("chat.freenode.net",6667);
+    login();
+
+    while(true) {
+      sleep(100);
+      fromServer.clear();
+      int numberBytesRead = serverConnection.read(fromServer);
+
+      if (numberBytesRead == -1) {
+	System.out.println("IRC connection died.");
+	serverConnection = createConnection("chat.freenode.net",6667);
+	login();
+      }
+      else {
+	String incoming = "";
+	fromServer.flip();
+	byte[] bytes = new byte[fromServer.remaining()];
+	fromServer.get(bytes);
+
+	if(bytes.length > 0) {
+	  String s = new String(bytes,"UTF-8");
+	  incoming = incoming + s;
+	  //TODO: Handle incoming message
+	  System.out.println(incoming);
+	}
+      }
+    }
   }
 }
