c5dfafe8831a9066a5e0c5e20b3b9e18d97e589d
Derek Hoagland <thegrickit@gmail.com>
2011-11-20 20:20:28 -0500

    Moar message parsing

--- MessageParser.java
+++ MessageParser.java
@@ -8,34 +8,43 @@ import java.util.regex.Matcher;
 
 public class MessageParser {
   private static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+    private static String pipe_id = "";
+    private static String bot_name = "";
+    private static String incoming_message = "";
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    String pipe_id = stdin.readLine().trim();
-    String bot_name = stdin.readLine().trim();
-    String incoming_message = stdin.readLine().trim();
+    pipe_id = stdin.readLine().trim();
+    bot_name = stdin.readLine().trim();
+    incoming_message = stdin.readLine().trim();
 
     if(pipe_id.equals("fork20")) System.out.println("send_server_message>JOIN ##Gambot");
 
     String valid_nick_characters = "A-Za-z0-9[\\]\\`_^{}|-";
-    String valid_chan_characters = "#"+valid_nick_characters;
-    String valid_human_sender_regex = "([."+valid_nick_characters+"]+)!~?([."+valid_nick_characters+"]+)@(.+?)";
+    String valid_chan_characters = "#A-Za-z0-9[\\]\\`_^{}|-";
+    String valid_human_sender_regex = "([.A-Za-z0-9[\\]\\`_^{}|-]+)!~?([.A-Za-z0-9[\\]\\`_^{}|-]+)@(.+?)";
 
     Matcher matcher;
 
     if((matcher = Pattern.compile("^PING(.*)$").matcher(incoming_message)).matches()) {
       on_ping(matcher.group(1),"","","","","");
     }
-
+    else if((matcher = Pattern.compile("^:([.A-Za-z0-9[\\\\]`_^{}|-]+)!~?([.A-Za-z0-9[\\\\]`_^{}|-]+)@(.+?) (PRIVMSG) ([#A-Za-z0-9[\\\\]`_^{}|-]+) :(.+)$").matcher(incoming_message)).matches()) {
+      if(matcher.group(5).equals(bot_name)) { on_private_message(matcher.group(1),matcher.group(2),matcher.group(3),matcher.group(4),matcher.group(5),matcher.group(6)); }
+      else { on_public_message(matcher.group(1),matcher.group(2),matcher.group(3),matcher.group(4),matcher.group(5),matcher.group(6)); }
+    }
   }
 
   public static void on_ping(String sender, String account, String hostname, String command, String target, String message) {
     System.out.println("send_server_message>PONG"+sender);
   }
   public static void on_private_message(String sender, String account, String hostname, String command, String target, String message) {
-
+    //We do a bit of redirection on private messages so they can be processed the same as public ones.
+    //The channel is the sender of the private message
+    //The message is prefixed with the bot's name
+    on_public_message(sender,account,hostname,command,sender,bot_name+": "+message);
   }
   public static void on_public_message(String sender, String account, String hostname, String command, String target, String message) {
-
+    System.out.println("send_server_message>PRIVMSG "+target+" :"+message);
   }
   public static void on_private_notice(String sender, String account, String hostname, String command, String target, String message) {
 
